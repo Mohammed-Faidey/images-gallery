ed36271fb326506b138ff13bf814b922bccbe7a3
Mohammed-Faidey <mohammedmostafa1998@hotmail.com>
2024-02-24 13:54:02 +0300

    Enabling ESLint Errors Fixing on File Save

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,31 +3,36 @@ import Header from './components/Header';
 import Search from './components/Search';
 import { useState } from 'react';
 
-
 function App() {
   const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH;
   const [word, setWord] = useState('');
-  
-  const handleSearchSubmit = (e) =>{
+
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(UNSPLASH_KEY);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((res) => res.json())
-    .then((data) =>{
-      console.log(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     setWord('');
-  }
+  };
 
   //console.log(process.env);
 
   return (
     <div className="App">
       <Header title="Images Gallery"></Header>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}></Search>
+      <Search
+        word={word}
+        setWord={setWord}
+        handleSubmit={handleSearchSubmit}
+      ></Search>
     </div>
   );
 }
